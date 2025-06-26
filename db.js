43ed09308003298b68b93bbc3a580bc3c379fdcb
Pragathi G <pragathign03@gmail.com>
2025-06-26 22:27:38 +0530

    updated

--- db.js
+++ db.js
@@ -5,11 +5,11 @@ dotenv.config()
 //const mongoURL= process.env.MONGODB_URL_LOCAL
 const mongoURL= process.env.MONGODB_URL
 
-mongoose.connect(mongoURL)
-// , {
-//     useNewURLParser: true,
-//     useUnifiedTopology: true
-// })
+mongoose.connect(mongoURL , {
+     useNewURLParser: true,
+     useUnifiedTopology: true,
+     ssl: true
+ })
 
 const db= mongoose.connection;
 
